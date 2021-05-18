58fdb469f13f007fc0245015b6cd2e9119991290
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-05-18 23:34:59 +0200

    MAINT: align with lookup server v 0.17.2 for jwt-extended >= 4

--- dtool_lookup_server_dependency_graph_plugin/__init__.py
+++ dtool_lookup_server_dependency_graph_plugin/__init__.py
@@ -31,7 +31,7 @@ graph_bp = Blueprint("graph", __name__, url_prefix="/graph")
 
 
 @graph_bp.route("/lookup/<uuid>", methods=["GET"])
-@jwt_required
+@jwt_required()
 def lookup_dependency_graph_by_default_keys(uuid):
     """List the datasets within the same dependency graph as <uuid>.
     If not all datasets are accessible by the user, an incomplete, disconnected
@@ -45,7 +45,7 @@ def lookup_dependency_graph_by_default_keys(uuid):
 
 
 @graph_bp.route("/lookup/<uuid>", methods=["POST"])
-@jwt_required
+@jwt_required()
 def lookup_dependency_graph_by_custom_keys(uuid):
     """List the datasets within the same dependency graph as <uuid>.
     If not all datasets are accessible by the user, an incomplete, disconnected
@@ -60,7 +60,7 @@ def lookup_dependency_graph_by_custom_keys(uuid):
 
 
 @graph_bp.route("/config", methods=["GET"])
-@jwt_required
+@jwt_required()
 def plugin_config():
     """Return the JSON-serialized dependency graph plugin configuration."""
     username = get_jwt_identity()
